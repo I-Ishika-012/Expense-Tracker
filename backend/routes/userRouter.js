1291ba89a0717829ccc5db30be34acaab0d55a4a
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-19 00:56:29 +0530

    Update userRouter.js

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const router = express.Router();
+const userRouter = express.Router();
 const usersController = require("../controllers/usersCtrl");
 
-router.post("/api/v1/register", usersController.register);
-router.post("/api/v1/login", usersController.login);
 
-module.exports = router;
+
+userRouter.post("/api/v1/register", usersController.register);
+// userRouter.post("/api/v1/login", usersController.login);
+
+module.exports = userRouter;
