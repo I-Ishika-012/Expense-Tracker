0333e8078fe2ec6ac659b0ca75ecea5587fe44da
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-19 00:56:06 +0530

    Update usersCtrl.js

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -1,37 +1,13 @@
 const asyncHandler = require("express-async-handler");
-const User = require("../models/userModel");
+const User = require("../model/User");
 
 //! User Registration
 
 const usersController = {
-    //? user registration
+    
     register: asyncHandler(async (req, res) => {
-        try {
-            //? get user input
-            const { username, email, password } = req.body;
-
-            //? check if user exists
-            const user = await User.findOne({ email });
-
-            //? if user exists
-            if (user) {
-                return res.status(400).json({ message: "User already exists" });
-            }
-
-            //? create new user
-            const newUser = new User({ username, email, password });
-
-            //? save user to database
-            await newUser.save();
-
-            //? return response
-            res.status(201).json({ message: "User registered successfully" });
-        } catch (error) {
-            return res.status(500).json({ message: error.message });
-        }
-    },)
-    //?login
-    //?profile
+        res.json({ msg: "register" });
+    }),
 };
 
 module.exports = usersController;
