48c7d5a4e26946b5a2812a2403a78451d7798c0f
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-19 00:57:10 +0530

    Update app.js

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const userRouter = require("./routes/userRouter");
+
+app.use(express.json());
+
+//?routes
+app.use("/", userRouter);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use("/api/v1", require("./routes/userRouter"));
+// app.use("/api/v1", require("./routes/transactionRouter"));
+// app.use("/api/v1", require("./routes/categoryRouter"));
+// app.use("/api/v1", require("./routes/authRouter"));
+
+//?start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
