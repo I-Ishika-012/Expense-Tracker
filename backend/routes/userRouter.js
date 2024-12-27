02645ce417da317ae382cc3adaf654379e4120f3
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-12-27 18:12:10 +0530

    Update userRouter.js

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -10,4 +10,7 @@ userRouter.post("/api/v1/users/register", usersController.register);
 //! Login User
 userRouter.post("/api/v1/users/login", usersController.login);
 
+//! Get User Profile
+userRouter.get("/api/v1/users/profile", usersController.profile);
+
 module.exports = userRouter;
