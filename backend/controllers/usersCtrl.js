553d80612c000b34cb3785d5f7c6ea6056a2c847
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-12-27 16:24:20 +0530

    Update usersCtrl.js

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -1,14 +1,38 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 
 //! User Registration
 
 const usersController = {
-    //! User Registration
+    //! Register User
     register: asyncHandler(async (req, res) => {
         //?get payload/data from request body
         const { username, email, password } = req.body;
-        res.json({ msg: "register" });
+    //! Validate User
+    if (!username || !email || !password) {
+        throw new Error("Please fill in all fields");
+    }
+    //! Check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        throw new Error("User already exists");
+    }
+    //! Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //! Create & Save User
+    const userCreated = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+    //! Send Response
+    res.status(201).json({
+        _id: userCreated._id,
+        username: userCreated.username,
+        email: userCreated.email,
+    });
     }),
     //! Login User
     //! Get User Profile
