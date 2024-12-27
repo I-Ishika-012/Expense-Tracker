460ecbfac16d72d3608111181e1c15b1d8384865
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-12-27 18:19:40 +0530

    Update userRouter.js

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -4,6 +4,8 @@ const usersController = require("../controllers/usersCtrl");
 
 const userRouter = express.Router();
 
+const isAuthenticated = require("../middlewares/isAuth");
+
 //! Register User
 userRouter.post("/api/v1/users/register", usersController.register);
 
@@ -11,6 +13,6 @@ userRouter.post("/api/v1/users/register", usersController.register);
 userRouter.post("/api/v1/users/login", usersController.login);
 
 //! Get User Profile
-userRouter.get("/api/v1/users/profile", usersController.profile);
+userRouter.get("/api/v1/users/profile", isAuthenticated, usersController.profile);
 
 module.exports = userRouter;
